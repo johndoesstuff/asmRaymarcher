4c4188ffeec8a00402eaead581cb85eb605ebd67
johndoesstuff <johndoesstuff03@gmail.com>
2024-12-30 22:24:45 -0500

    loops??

--- main.asm
+++ main.asm
@@ -9,13 +9,20 @@ section .data
 	sp_y: db 0
 	sp_z: db 0
 
+	; looping
+	count: db 0
+
+	; screen size
 	sc_col: dw 0
 	sc_row: dw 0
 
-	fmt db 'Screen width: %u  Screen height: %u', 10, 0
+	whmsg db 'Screen width: %u  Screen height: %u', 10, 0
+
+	lpmsg db 'looped', 0
 
 section .bss
 	sz RESW 4
+	ray_pointers RESQ 1
 
 section .text
 global _start
@@ -35,16 +42,30 @@ _start:
 	mov WORD [sc_col], si
 	mov WORD [sc_row], di
 
-	mov rdi, fmt ; print screen size using printf
+	mov rdi, whmsg ; print screen size using printf
 	movzx rsi, WORD [sc_col]
 	movzx rdx, WORD [sc_row]
 	xor eax, eax
 	call printf
 
-	mov rdi, [sc_col] ; malloc screen cols
+	mov rdi, [sc_col] ; malloc screen cols to ray pointers
 	call malloc
+	mov [ray_pointers], rax
 
-	mov rbx, rax
+	; initialize row loop
+	movzx eax, WORD [sc_row]
+	mov [count], al
+
+allocate_ray_rows:
+
+	
+	mov rdi, lpmsg ; print screen size using printf
+	xor eax, eax
+	call printf
+
+	dec BYTE [count]	
+	cmp BYTE [count], 0
+	jg allocate_ray_rows
 
 	xor edi, edi
 	call exit
